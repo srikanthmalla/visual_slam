6437c207c51ee9819257eb526258962b8c64b3c0
srikanthmalla <mallasrikanth004@gmail.com>
2022-03-06 15:44:51 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Visual SLAM
 
+## Requirements
+
+1. ROS (tested with kinetic)
+
+2. `sudo pip install kitti2bag` (for conversion to rosbag)
+
+
+## Run the process
+
 1. Clone the repo to your workspace `https://github.com/srikanthmalla/visual_slam.git`
 
 2. cd `visual_slam` (go in to the package)
@@ -10,3 +19,14 @@
 
 5. Run `roslaunch visual_slam visual_slam.launch`
 
+## RunTime
+
+Input data (camera) is 9.690 hz
+
+With cpu_flow (KLT)~ program runs at 3hz
+
+For using gpu_flow from openCV, please check `setup_notes.txt`
+
+## To remember
+
+* In opencv z is considered forward, but x is considered backward in ROS (kitti) and z down.
--- download_dataset.sh
+++ download_dataset.sh
@@ -1,5 +1,7 @@
 #install kitti2bag
-# sudo pip install kitti2bag
+sudo pip install kitti2bag
+mkdir datasets
+cd datasets
 
 # echo "downloading visual odometry dataset of size 22 GB"
 # wget http://kitti.is.tue.mpg.de/kitti/data_odometry_gray.zip -o 2011_09_26_odometry_gray.zip
@@ -9,18 +11,11 @@
 # kitti2bag -t 2011_09_26 raw_synced .
 
 #small dataset to test
-# echo "downloading small datset and converting to rosbag"
-# wget http://kitti.is.tue.mpg.de/kitti/raw_data/2011_09_26_drive_0002/2011_09_26_drive_0002_sync.zip
-# wget http://kitti.is.tue.mpg.de/kitti/raw_data/2011_09_26_calib.zip
-# unzip 2011_09_26_drive_0002_sync.zip
-# unzip 2011_09_26_calib.zip
-# kitti2bag -t 2011_09_26 -r 0002 raw_synced .
-
-# already created rosbags
-mkdir datasets
-
-cd datasets
-
-wget https://www.dropbox.com/s/jgxqtwfsqtoavz6/2011_09_26_0001.bag
-
+echo "downloading small datset and converting to rosbag"
+## recently kitti is moved to aws
+wget https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/2011_09_26_drive_0002/2011_09_26_drive_0002_sync.zip
+wget https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/2011_09_26_calib.zip
+unzip 2011_09_26_drive_0002_sync.zip
+unzip 2011_09_26_calib.zip
+kitti2bag -t 2011_09_26 -r 0002 raw_synced .
 cd ..
